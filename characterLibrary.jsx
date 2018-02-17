81b3d59a08098b14a2787ac04303244d962017af
ChoiChoi <seunghyuk.choi@stonybrook.edu>
2018-02-16 23:55:13 -0500

    added node_modules to .gitignore_global

--- characterLibrary.jsx
+++ characterLibrary.jsx
@@ -5,9 +5,23 @@ class CharacterLibrary extends React.Component {
 		super(props)
 		this.state=props;
 	}
+
+	handleClick = (buttonName) => {
+		let i = 0;
+		for (i=0; i < this.state.characters.length; i++) {
+
+		}
+	}
+
 	render(){
-		return <div></div>;
+		return <div></div>
+            <button
+				onclick={this.handleClick}>
+					Click me
+				</button>;
 	}
 }
 
-export CharacterLibrary;
+
+
+export {CharacterLibrary};
